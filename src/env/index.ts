ba33ebd218ad801dcf7a9efa7d6d18f1aa257e76
LuisLoschi <gustavoloschi362@gmail.com>
2024-06-24 22:36:23 -0300

    new: config env with zod

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+// Acessar as variaveis
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+})
+
+// faz validação
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('😥Invalid enviroment variables!', _env.error.format())
+
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
