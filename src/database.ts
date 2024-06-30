ff1259d51eb941f373d3d1785cec7cdd3eda4950
LuisLoschi <gustavoloschi362@gmail.com>
2024-06-30 19:07:31 -0300

    Fix: config deploy

--- src/database.ts
+++ src/database.ts
@@ -3,14 +3,12 @@ import { env } from './env'
 
 // console.log(process.env)
 
-const verifySqliteOrPostgreesConnection =
-  env.DATABASE_CLIENT === 'sqlite'
-    ? { filename: env.DATABASE_URL }
-    : env.DATABASE_URL
-
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: verifySqliteOrPostgreesConnection,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? { filename: env.DATABASE_URL }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
