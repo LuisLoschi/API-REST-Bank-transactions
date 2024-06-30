53c1cdb6953d6ffa20b30773f46e80297b06e5ba
LuisLoschi <gustavoloschi362@gmail.com>
2024-06-30 08:17:51 -0300

    new: Config db test

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,6 +1,7 @@
-import { it, expect, beforeAll, afterAll, describe } from 'vitest'
+import { it, expect, beforeAll, afterAll, describe, beforeEach } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
+import { execSync } from 'child_process'
 
 describe('Transaction routes', () => {
   beforeAll(async () => {
@@ -11,6 +12,11 @@ describe('Transaction routes', () => {
     await app.close()
   })
 
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
   it('should be able to create a new transaction', async () => {
     const response = await request(app.server).post('/transactions').send({
       title: 'New Transaction',
@@ -19,5 +25,30 @@ describe('Transaction routes', () => {
     })
 
     expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 1000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const response = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies || [])
+
+    expect(response.statusCode).toEqual(200)
+
+    expect(response.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 1000,
+      }),
+    ])
   })
 })
