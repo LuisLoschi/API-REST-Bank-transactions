2d87fd0e0698db9acd4665148abdffbd0b105445
LuisLoschi <gustavoloschi362@gmail.com>
2024-06-30 18:52:39 -0300

    Fix: knex command to deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'test') {
 // Acessar as variaveis
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333), // coerce convert variable type
 })
