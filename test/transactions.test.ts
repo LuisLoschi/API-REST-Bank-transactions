8c50d44d9396fb50e62948610add240112dd4958
LuisLoschi <gustavoloschi362@gmail.com>
2024-06-30 12:13:46 -0300

    Config: preparing to deploy

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -51,4 +51,66 @@ describe('Transaction routes', () => {
       }),
     ])
   })
+
+  it('should be able to get specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 1000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies || [])
+
+    expect(listTransactionResponse.statusCode).toEqual(200)
+
+    const transactionId = listTransactionResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies || [])
+      .expect(200)
+
+    expect(getTransactionResponse.body.transactions).toEqual(
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 1000,
+      }),
+    )
+  })
+
+  it('should be able to get the summary of the transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit Transaction',
+        amount: 1000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies || [])
+      .send({
+        title: 'Debit Transaction',
+        amount: 500,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies || [])
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 500,
+    })
+  })
 })
