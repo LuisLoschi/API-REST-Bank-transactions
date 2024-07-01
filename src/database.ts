e16990f4339fcc617b952f647783e6f56d074aaa
LuisLoschi <gustavoloschi362@gmail.com>
2024-06-30 23:36:47 -0300

    Fix: config deploy

--- src/database.ts
+++ src/database.ts
@@ -1,18 +1,18 @@
-import { knex as setupKnex, Knex } from 'knex'
+import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
-
-// console.log(process.env)
 
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
   connection:
     env.DATABASE_CLIENT === 'sqlite'
-      ? { filename: env.DATABASE_URL }
+      ? {
+          filename: env.DATABASE_URL,
+        }
       : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './db/migration',
+    directory: './db/migrations',
   },
 }
 
